ff1d72a67d9c3adf229cf9aa7ccee0e963d1ab36
Chetan <ankireddychetan2004@gmail.com>
2025-11-27 11:45:24 +0530

    Delete tasks directory

--- tasks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AnalyzeTasksAPIView, SuggestTasksAPIView
-
-urlpatterns = [
-    path("analyze/", AnalyzeTasksAPIView.as_view(), name="analyze-tasks"),
-    path("suggest/", SuggestTasksAPIView.as_view(), name="suggest-tasks"),
-]
